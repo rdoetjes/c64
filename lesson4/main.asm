10681c4f30e8a35be78848b15a5eff25ac13920c
Raymond Doetjes <rdoetjes@phonax.com>
2022-08-24 17:44:10 +0200

    comments and zero page

--- lesson4/main.asm
+++ lesson4/main.asm
@@ -1,11 +1,9 @@
 .const SCREEN = $0400
 
-* = $01000
-
 BasicUpstart2(main)
 
 main:
-  lda #242                    //number to turn into hex
+  lda #16                    //number to turn into hex
   jsr printHex                //call the hex conversion (result will be in printHex_result)
   lda printHex_result         //get first hex character
   sta SCREEN                  //print that hex character on position 0
@@ -20,50 +18,30 @@ printHex:
   // the calculated result is stored (self modifying code, so we can easily use it where and how we want)
   printHex_result:    .byte '0', '0'
 !:
-  //we need to save the a (that holds the value to print) as we need to txa to save x
-  //so let's store it in the result, saves a byte and doesn't require complex stack handling
-  sta printHex_result
-
-  //safe the x register on the stack, because we will be changing it's contents
-  txa        
+  sta $fb                   //we need to save the a as we need to txa to save x, so we use zero page for a second
+  txa                       //safe the x register on the stack, because we will be changing it's contents 
   pha
+  lda $fb                   //get the A value from zero page again
+  pha                       // we need to save a copy of a on the stack, because we will and it and globber it
+  
+  and #15                   // and the lower nibble, so we know what the lower past of the hex value is
+  tax                       //set the result from the and in x
+  lda printHex_hextable, x  //lookup in the printHex_hextable array the right printHex_hextable character for index in x register
+  sta printHex_result + 1   //store that in the result + 1 position (lowest of the two printHex_hextable nibble)
 
-  //get the A value again, that is the number to print in printHex_hextable
-  lda printHex_result
-  // we need to save a copy of a on the stack, because we will and it and globber it
-  pha
-  //And let's set it back to 0 char (we are decent and careful people, especially when it comes to self modifying code)
-  ldx #'0'
-  stx printHex_result
-
-  // and the lower nibble
-  and #15
-  //set the result from the and in x
-  tax
-  //lookup in the printHex_hextable array the right printHex_hextable character for index in x register
-  lda printHex_hextable, x
-  //store that in the result + 1 position (lowest of the two printHex_hextable nibble)
-  sta printHex_result + 1
-
-  // pop the a so we have the actual number again in a
-  pla
-  // and the high nibble
-  and #240
-  //shift that result 4 times to the right, into the lower nibble
-  lsr
+  pla                       // pop the a so we have the actual number again in a
+  and #240                  // and the high nibble
+  
+  lsr                       //shift that result 4 times to the right, into the lower nibble
   lsr
   lsr
   lsr
 
-  // transfer that nibble from a into x
-  tax
-  // get the correct printHex_hextable chracter for index x
-  lda printHex_hextable, x
-  //store that printHex_hextable character into printHex_result (the highest nibble)
-  sta printHex_result 
+  tax                       // transfer that nibble from a into x
+  lda printHex_hextable, x  // get the correct printHex_hextable chracter for index x
+  sta printHex_result       //store that printHex_hextable character into printHex_result (the highest nibble)
 
-  // restore x
-  pla
+  pla                       // restore x
   tax
 
-  rts
+  rts 
