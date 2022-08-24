c0b0aaff7c08454c1607df2b66877b622b576dcb
Raymond Doetjes <rdoetjes@phonax.com>
2022-08-24 08:24:25 +0200

    lesson02

--- lesson02/main.asm
+++ lesson02/main.asm
@@ -1,5 +1,5 @@
-.const BRCOLOR = $d020
-.const BGCOLOR = $d021
+.const BRCOLOR = 53280
+.const BGCOLOR = 53281
 
 BasicUpstart2(main)
 
@@ -9,8 +9,8 @@ main:
 //10 poke 53280,peek(53280)-1
 //20 poke 53281,peek(53281)-1
 //30 goto 10
-
-  dec BGCOLOR
   dec BRCOLOR
+  dec BGCOLOR
   jmp main
+
   rts
