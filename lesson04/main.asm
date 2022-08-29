7878da07290b5f19a1cc124b105231036238ea53
Raymond Doetjes <rdoetjes@phonax.com>
2022-08-29 17:01:35 +0200

    comments

--- lesson04/main.asm
+++ lesson04/main.asm
@@ -1,38 +1,40 @@
-.const SID_VOICE3_LB = $d40e
-.const SID_VOICE3_CTRL = $d412
-.const SID_WAV_NOISE = $80
-.const SID_OSC_3_RO = $d41b
-.const KERNAL_CHROUT = $FFD2 
-* = $1000
+
+// 10 PRINT CHR$ (205.5 + RND (1)); : GOTO 10
+// in assmebly
+
+.const SID_VOICE3_LB = $d40e    //voice 3 lowbyte for requence
+.const SID_VOICE3_CTRL = $d412  //voice 3 control register (to select wave type)
+.const SID_WAV_NOISE = $80      //128 dec, when put in voice3 control register it will select noise wave type
+.const SID_OSC3_RO = $d41b      //this will contain the amplitude value of the noise at moment of reading (because wave is noise it's random)
+.const KERNAL_CHROUT = $ffd2    //print chrout kernal call
 
 BasicUpstart2(main)
 
 main:
-  ldx #$00
-  stx $fb
-  jsr setupSid4Noise
-
+  jsr setupSid4Noise            //setup the voice 3 for noise generation 
 loop:
-  lda SID_OSC_3_RO  // get random value from noise in A
-  jsr getMazeChar   // get randomly either a / or \
-  jsr KERNAL_CHROUT // use kernal print char to screen (not the fastest way!, but that's not the point of this lesson ;) )
-  jmp loop          // loop forever
+  lda SID_OSC3_RO               //load random voice amplitude value into a
+  jsr getMazeChar               // depending if random value is odd or even we return in a / or \
+  jsr KERNAL_CHROUT             // print char in a to the screen
+  jmp loop                      // loop forever
 
-setupSid4Noise:
-  lda #$ff            // highest frequency in low byte and high byte (gives random values faster)
-  sta SID_VOICE3_LB   // voice 3 frequency low byte
-  sta SID_VOICE3_LB+1 // voice 3 frequency high byte
-  lda #SID_WAV_NOISE   // waveform is set to noise
-  sta SID_VOICE3_CTRL // set the wave form to 80 (noise)
-  rts
-
-getMazeChar:
-  //and the random number with 1 if it's 0 then set a to / else \
-  and #$01
-  beq !+   //if a and 1 == 0 set a to /
-  lda #110 //else set a to \
+//return / if value in a is even and return \ if value in a is odd
+getMazeChar:                    
+  and #$1                       // and a with 1, this will return either 0 or 1 depending whether value in a is even or odd
+  beq !+                        //if a and 1 == 0 then a = /
+  lda #109                      //else a = \
   jmp !++
 !:
-  lda #109 // set to /
-!: 
+  lda #110
+!:
+  rts       
+
+//voice 3 is the only voice with noise generator and we enabled it with highest frequency
+//in order to get fastest random numbers
+setupSid4Noise:                 
+  lda #$ff                      // load a with 255, which is highest frequence when put in, voice lb and voice hb       
+  sta SID_VOICE3_LB             // set frequence in frequency low byte to 255 (highest frequence)
+  sta SID_VOICE3_LB+1           // set frequence in frequency low byte to 255 (highest frequence)
+  lda #SID_WAV_NOISE            // load a wuth $80 (128) which is noise wave when store in SID_VOICE3_CTRL
+  sta SID_VOICE3_CTRL           // set voice 3 control register to play a noise wave
   rts
