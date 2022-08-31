250e7fedf5479aed3fa5033fe1b48aa51fd6e948
Raymond Doetjes <rdoetjes@phonax.com>
2022-08-31 15:18:19 +0200

    cleanup lesson 7

--- lesson07/main.asm
+++ lesson07/main.asm
@@ -7,18 +7,10 @@ BasicUpstart2(main)
 
 main:
   jsr setup
-
-  ldx #00
-  sta frameCount
-  sta frameCount+1
-
   eventLoop:
+    jsr text        //write text again for later when we turn into into marquee
     jsr colorCycle
-
-    jsr text
-
     jsr frameWait
-
     jmp eventLoop
 rts
 
@@ -27,14 +19,6 @@ frameWait:
  !raster:
    cmp VIC.RASTER_LINE
    bne !raster-
-   lda frameCount
-   adc #1 
-   sta frameCount
-   bcs !add+
-   jmp !end+
-!add:
-   adc frameCount+1
-   sta frameCount+1
 !end:    
 rts
 
@@ -68,59 +52,58 @@ colorCycle:
 rts
 
 setup:
-   jsr pointToRAMCharSet
+  jsr pointToRAMCharSet
     
-    lda #$00
-    sta VIC.BORDER_COLOR
-    sta VIC.SCREEN_COLOR
+  lda #$00
+  sta VIC.BORDER_COLOR
+  sta VIC.SCREEN_COLOR
 
-    jsr Cls
+  jsr Cls
 rts
 
 text:
-   ldx #$00
+  ldx #$00
 !write:   
-   lda text1, X
-   cmp #$00
-   beq !end+
+  lda text1, X
+  cmp #$00
+  beq !end+
    
-   sta VIC.SCREEN + OFFSET, x
-   adc #31
-   sta VIC.SCREEN + OFFSET + 40, x 
-   inx
+  sta VIC.SCREEN + OFFSET, x
+  adc #31
+  sta VIC.SCREEN + OFFSET + 40, x 
+  inx
 
-   jmp !write-
+  jmp !write-
 !end:
 rts
 
 //Point to Charset in $3000
 pointToRAMCharSet:
-    lda VIC.MEMORY_SETUP
-    and #240
-    ora #12
-    sta VIC.MEMORY_SETUP
+  lda VIC.MEMORY_SETUP
+  and #240
+  ora #12
+  sta VIC.MEMORY_SETUP
 rts
 
 //Clear the VIC.SCREEN in 4 250 character blocks
 Cls:
-    ldx #250
-    lda #96 //blank tile in this map
-  !:
-    dex
-    sta VIC.SCREEN, x
-    sta VIC.SCREEN+250, x
-    sta VIC.SCREEN+500, x
-    sta VIC.SCREEN+750, x
-    bne !-
-rts
+  ldx #250
+  lda #96 //blank tile in this map
+!:
+  dex
+  sta VIC.SCREEN, x
+  sta VIC.SCREEN+250, x
+  sta VIC.SCREEN+500, x
+  sta VIC.SCREEN+750, x
+  bne !-
+  rts
 
 text1:
-    .text "the commodore rules it is wonderful"
-    .byte 00
-text1end:
+  .text "are you keeping up with the commodore"
+  .byte 00
 
 frameCount:
-    .byte 00, 00
+  .byte 00, 00
 
 gradientOffset:
   .byte 00
