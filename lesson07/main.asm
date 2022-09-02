5eeac47ad3865f7de5d8a4cbfe5a03c23ea3f449
Raymond Doetjes <rdoetjes@phonax.com>
2022-09-02 12:36:25 +0200

    colors refactored and cleaned up

--- lesson07/main.asm
+++ lesson07/main.asm
@@ -1,55 +1,67 @@
-*=$1000
-#import "memorymap.asm"
+#import "memorymap.asm"     //include some of the the constants aka lables for VIC and SID so we don't need to do that manually every time.
 
-.const OFFSET = $140
+.const OFFSET = $190        //offset for where we want to begin writing the string
 
 BasicUpstart2(main)
 
 main:
-  jsr setup
+  jsr setup                 // setup routine will tell VIC where to find custom charset and clears the screen
   eventLoop:
-    jsr text        //write text again for later when we turn into into marquee
-    jsr colorCycle
-    jsr frameWait
+    jsr text                // write text over and over again, as later on we will make this scroll    
+    jsr fillColorOnline     // cycle the colours in the text
+    jsr frameWait           // wait for a frame
     jmp eventLoop
-rts
 
 frameWait:
-   lda #$f0
+   lda #$ff                 // loop until line 255 is drawn by the VIC
  !raster:
-   cmp VIC.RASTER_LINE
-   bne !raster-
+   cmp VIC.RASTER_LINE      // compare current raster line with #$ff
+   bne !raster-             // current rasterline not #$ff jump to raster, this way we sync drawing with raster line
 !end:    
 rts
 
-colorCycle:
-  ldx gradientOffset
-  inx
-  cpx #14
-  beq !resetX+
-  jmp !storeX+
-!resetX:
-  ldx #$0
-!storeX:
-  stx gradientOffset
-  
-  ldy #$ff  //number of characters + 1
-!fetchColor:
-  dey
-  lda gradientColor, x
-  sta VIC.COLOR_RAM + OFFSET, y
-  sta VIC.COLOR_RAM + OFFSET + 40, y
-  inx
+getGradientColor:
+  ldx gradientOffset 
   cpx #14
   beq !+
-  cpy #$00
-  beq !end+
-  jmp !fetchColor-
+  jmp !++
 !:
-  ldx #$0
-  jmp !fetchColor-
-!end:    
+  ldx #$00
+  sta gradientOffset
+!:
+  lda gradientColor, x
+  inx
+  stx gradientOffset
+  rts
+
+fillColorOnline:
+  ldy #39
+!:
+  jsr getGradientColor
+  sta VIC.COLOR_RAM + OFFSET, y
+  sta VIC.COLOR_RAM + OFFSET + 40, y
+  dey
+  cpy #$ff
+  bne !-
+  rts
+
+colorCycle:
+ 
+    
 rts
+
+//Clear the VIC.SCREEN in 4 250 character blocks
+clearScreen:
+  ldx #250
+  lda #32 //blank tile in this map
+!:
+  dex
+  sta VIC.SCREEN, x
+  sta VIC.SCREEN+250, x
+  sta VIC.SCREEN+500, x
+  sta VIC.SCREEN+750, x
+  bne !-
+  rts
 
 setup:
   jsr pointToRAMCharSet
@@ -58,7 +70,7 @@ setup:
   sta VIC.BORDER_COLOR
   sta VIC.SCREEN_COLOR
 
-  jsr Cls
+  jsr clearScreen
 rts
 
 text:
@@ -77,7 +89,7 @@ text:
 !end:
 rts
 
-//Point to Charset in $3000
+//Point to Charset in $3000, we use 3000 because we will later set a sid tune on 2000
 pointToRAMCharSet:
   lda VIC.MEMORY_SETUP
   and #240
@@ -85,31 +97,19 @@ pointToRAMCharSet:
   sta VIC.MEMORY_SETUP
 rts
 
-//Clear the VIC.SCREEN in 4 250 character blocks
-Cls:
-  ldx #250
-  lda #96 //blank tile in this map
-!:
-  dex
-  sta VIC.SCREEN, x
-  sta VIC.SCREEN+250, x
-  sta VIC.SCREEN+500, x
-  sta VIC.SCREEN+750, x
-  bne !-
-  rts
-
 text1:
-  .text "are you keeping up with the commodore"
+  .text " are you keeping up with the commodore!"
   .byte 00
 
-frameCount:
-  .byte 00, 00
+text_color_delay:
+  .byte 00
 
 gradientOffset:
   .byte 00
 
 gradientColor:
-  .byte $01, $07, $0f, $0a, $0c, $04, $0b, $06, $06, $04, $0c, $0a, $0f, $07
+  .byte $07, $07, $0f, $0a, $0c, $04, $0b, $06, $06, $04, $0c, $0a, $0f, $0b
+
 //load charset in $3000
 *=$3000
 #import "charset_1.asm"
