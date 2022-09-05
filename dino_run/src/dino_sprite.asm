642e8c9350b755b1c8e2b7907e5a9ae8df966492
Raymond Doetjes <rdoetjes@phonax.com>
2022-09-05 20:32:46 +0200

    jump fixed

--- dino_run/src/dino_sprite.asm
+++ dino_run/src/dino_sprite.asm
@@ -192,117 +192,94 @@ dino_d_src:
 
 
 dino_j_src:
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $00, $00, $00
-	.byte $40, $00, $7E
-	.byte $C0, $00, $DF
-	.byte $C0, $00, $FF
-	.byte $FF, $FF, $FF
-	.byte $7F, $9F, $E0
-	.byte $3F, $EF, $78
-	.byte $1F, $FE, $00
-	.byte $0F, $FE, $00
-	.byte $01, $FB, $00
-	.byte $00, $60, $00
-	.byte $00, $30, $00
-	.byte $00
-
 	.byte $00, $03, $F8
-	.byte $00, $07, $7C
-	.byte $00, $0F, $7E
+	.byte $00, $07, $FE
+	.byte $00, $07, $FE
 	.byte $00, $0F, $FE
-	.byte $00, $0F, $FE
-	.byte $00, $0F, $E0
-	.byte $00, $0F, $F0
+	.byte $00, $0F, $80
+	.byte $00, $0F, $C0
+	.byte $00, $0F, $F8
 	.byte $00, $0F, $00
 	.byte $00, $1F, $80
 	.byte $00, $3F, $80
-	.byte $00, $7F, $90
-	.byte $20, $FF, $E0
-	.byte $61, $FF, $80
-	.byte $73, $CF, $80
-	.byte $7F, $B7, $00
-	.byte $3F, $FF, $00
-	.byte $1F, $FE, $00
-	.byte $0F, $FC, $00
-	.byte $01, $F8, $00
-	.byte $00, $70, $00
-	.byte $00, $D8, $00
+	.byte $00, $7F, $A0
+	.byte $00, $FF, $C0
+	.byte $01, $FF, $80
+	.byte $03, $FF, $80
+	.byte $07, $9F, $00
+	.byte $1F, $6F, $00
+	.byte $7F, $7E, $00
+	.byte $FF, $FC, $00
+	.byte $81, $F8, $00
+	.byte $00, $A0, $00
+	.byte $00, $A0, $00
 	.byte $00
 
 	.byte $00, $03, $F8
-	.byte $00, $07, $7C
+	.byte $00, $07, $FE
+	.byte $00, $06, $FE
 	.byte $00, $0F, $FE
-	.byte $00, $0F, $FE
-	.byte $00, $0F, $FE
-	.byte $00, $0F, $E0
+	.byte $00, $0F, $80
 	.byte $00, $0F, $F0
+	.byte $00, $0F, $F8
 	.byte $00, $0F, $00
 	.byte $00, $1F, $80
 	.byte $00, $3F, $80
-	.byte $00, $7F, $90
-	.byte $20, $FF, $E0
-	.byte $61, $FF, $80
-	.byte $73, $CF, $80
-	.byte $7F, $B7, $00
-	.byte $3F, $FF, $00
-	.byte $1F, $FE, $00
-	.byte $0F, $FC, $00
-	.byte $01, $F8, $00
-	.byte $00, $28, $00
-	.byte $00, $28, $00
+	.byte $00, $7F, $A0
+	.byte $00, $FF, $C0
+	.byte $01, $FF, $80
+	.byte $03, $FF, $80
+	.byte $07, $9F, $00
+	.byte $1F, $6F, $00
+	.byte $7F, $EE, $00
+	.byte $FF, $FC, $00
+	.byte $81, $F8, $00
+	.byte $00, $A0, $00
+	.byte $00, $A0, $00
 	.byte $00
 
 	.byte $00, $03, $F8
-	.byte $00, $07, $7C
+	.byte $00, $07, $FE
+	.byte $00, $06, $FE
 	.byte $00, $0F, $FE
-	.byte $00, $0F, $FE
-	.byte $00, $0F, $FE
-	.byte $00, $0F, $E0
-	.byte $00, $0F, $F0
+	.byte $00, $0F, $80
+	.byte $00, $0F, $C0
+	.byte $00, $0F, $F8
 	.byte $00, $0F, $00
 	.byte $00, $1F, $80
 	.byte $00, $3F, $80
-	.byte $00, $7F, $90
-	.byte $20, $FF, $E0
-	.byte $61, $FF, $80
-	.byte $73, $CF, $80
-	.byte $7F, $B7, $00
-	.byte $3F, $FF, $00
-	.byte $1F, $FE, $00
-	.byte $0F, $FC, $00
-	.byte $01, $F8, $00
-	.byte $00, $28, $00
-	.byte $00, $28, $00
+	.byte $00, $7F, $80
+	.byte $00, $FF, $E0
+	.byte $01, $FF, $80
+	.byte $03, $FF, $80
+	.byte $07, $9F, $00
+	.byte $1F, $6F, $00
+	.byte $7F, $6E, $00
+	.byte $FF, $FC, $00
+	.byte $81, $F8, $00
+	.byte $00, $A0, $00
+	.byte $00, $A0, $00
 	.byte $00
 
 	.byte $00, $03, $F8
-	.byte $00, $07, $7C
-	.byte $00, $0F, $7E
+	.byte $00, $07, $FE
+	.byte $00, $06, $FE
 	.byte $00, $0F, $FE
-	.byte $00, $0F, $FE
-	.byte $00, $0F, $E0
-	.byte $00, $0F, $F0
+	.byte $00, $0F, $80
+	.byte $00, $0F, $C0
+	.byte $00, $0F, $F8
 	.byte $00, $0F, $00
 	.byte $00, $1F, $80
 	.byte $00, $3F, $80
-	.byte $00, $7F, $90
-	.byte $20, $FF, $F0
-	.byte $61, $FF, $80
-	.byte $73, $CF, $80
-	.byte $7F, $B7, $00
-	.byte $3F, $FF, $00
-	.byte $1F, $FE, $00
-	.byte $0F, $FC, $00
-	.byte $01, $F8, $00
-	.byte $00, $28, $00
-	.byte $00, $28, $00
+	.byte $00, $7F, $A0
+	.byte $00, $FF, $C0
+	.byte $01, $FF, $80
+	.byte $03, $FF, $80
+	.byte $07, $9F, $00
+	.byte $1F, $6F, $00
+	.byte $7F, $6E, $00
+	.byte $FF, $FC, $00
+	.byte $81, $F8, $00
+	.byte $00, $A0, $00
+	.byte $00, $A0, $00
 	.byte $00
--- dino_run/src/game.asm
+++ dino_run/src/game.asm
@@ -1,25 +1,34 @@
 #import "macros.asm"
 
 gameLoop:
-  jsr readInput
-  jsr gameLogic
-  jsr draw
+  jsr readInput           // read the joystick input
+  jsr gameLogic           // process through the input and collision detection etc
+  jsr draw                // draw the new state
 
-  lda frame_counter
+  lda frame_counter       
 !:
   cmp frame_counter
   beq !-                  //sync to the frame (frame counter is incremented by raster interrupt)
   jmp gameLoop
 
 draw:
-  jsr dinoAnim
+  jsr dinoAnim            // change the dino sprites depending on the joystick input
   rts
 
-moveCharacter:
+//takes care of loading the right animation cycle and moving the player sprite
+moveCharacter:  
   lda playerState
   sta $0405
 
-  lda $ff         // copy of joystick
+  lda $ff         // up, jump
+  and #$01
+  beq !jump_up+ 
+
+  lda $ff         // fire button is also jump
+  and #$10
+  beq !jump_up+
+
+  lda $ff         // down is dug
   and #$02
   beq !dug+
   
@@ -27,26 +36,29 @@ moveCharacter:
   and #$04
   beq !left+
 
-  lda $ff         // left
+  lda $ff         // right
   and #$08
   beq !right+
 
-  lda $ff 
+  lda $ff         // neutral position (always moving)
   and #$80
   sta $0401
   beq !walk+
-  
-  lda $ff 
-  and #$01
-  sta $0401
-  beq !jump_up+
 
-  lda $ff 
-  and #$10
-  beq !jump_up+
   rts
 
-  !left:
+  !jump_up:    // 4 sprite (0-3) jump cycle, we prevent reloading when we don't need to hence the playerState
+    lda #$02
+    cmp playerState
+    bne !changeState+
+    rts
+    !changeState:
+      copy4Sprites(dino_j_src, dino_0_4)
+      lda #$02
+      sta playerState
+      rts
+
+  !left:          // move the player sprite to the left but not off screen!
     lda #25
     cmp $d000
     bne !move+
@@ -55,7 +67,7 @@ moveCharacter:
     dec $d000
     rts
 
-  !right:
+  !right:         // move the player sprite to the right but not use the high bit, we don't want the player to be too close to the spwaning enemy
     lda #$ff
     cmp $d000
     bne !move+
@@ -64,7 +76,7 @@ moveCharacter:
     inc $d000
     rts
 
-  !walk:
+  !walk:          // 4 sprite (0-3) walk cycle, we prevent reloading when we don't need to hence the playerState
     lda #$00
     cmp playerState
     bne !changeState+
@@ -75,7 +87,7 @@ moveCharacter:
       sta playerState
       rts
 
-  !dug:
+  !dug:         // 4 sprite (0-3) dug cycle, we prevent reloading when we don't need to hence the playerState
     lda #$01
     cmp playerState
     bne !changeState+
@@ -86,26 +98,18 @@ moveCharacter:
       sta playerState
       rts
 
-  !jump_up:
-    lda #$02
-    cmp playerState
-    bne !changeState+
-    rts
-    !changeState:
-      lda #$02
-      sta playerState
-      copy4Sprites(dino_j_src, dino_0_4)
-      rts
-
+// the game logic goes here
 gameLogic:
   jsr moveCharacter   //move character based on joystick input
   rts
-  
+
+ // read the joy stick and store it's value in zero page ff (saves 2 cycles for each position evaluation) 
 readInput:
   lda $dc00
   sta $ff
   rts
 
+// play the 4 step player animation sprites
 dinoAnim:
   inc dino_anim_count
   lda dino_anim_count
@@ -125,6 +129,7 @@ dinoAnim:
     sta $07f8
     rts
 
+// initialize the game and setup a raster interrupt that counts the frame_counter variable, which we will poll in game loop
 gameStart:
   sei                         // disable interrupts
 
@@ -152,11 +157,13 @@ gameStart:
   cli
   rts
 
+// initialize the game, entry point from main
 game:
   jsr gameStart
   jmp gameLoop
   rts
 
+// raster interrupt 1 that counts the frames
 rasterInt1:
   inc frame_counter
   lda frame_counter
@@ -165,18 +172,23 @@ rasterInt1:
   jmp $EA31 
   rti
 
+//flag to allow to exit the game
 quit_game:
   .byte $00
 
+// score in BCD
 score:
   .byte $00, $00, $00, $00, $00
 
+// frame counter
 frame_counter:
   .byte $00
 
+// dino sprite counter, each dino sprite has 4 animation steps (0-3)
 dino_anim_count:
   .byte 00
 
+// the current state the player is in (allows for more efficient processing and handling events that take several frames, such as jump)
 playerState:
   .byte 00
 
